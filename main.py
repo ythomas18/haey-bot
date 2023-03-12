471f96ff6ebd3b59ced8139b662f3d0b86bad430
yann thomas <yann.thomas01@etu.umontpellier.fr>
2023-03-12 09:11:35 +0100

    ça arrive

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 import praw
 from dotenv import load_dotenv
 import os
+from icalendar import Calendar
+from datetime import datetime, timedelta
 
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix='/')
 
@@ -91,7 +93,64 @@ async def start(ctx):
 @bot.command()
 async def stop(ctx):
     post_subreddit_posts.stop()
+    channel = bot.get_channel(1084085506272399370)
+    await channel.purge()
     await ctx.send('Bot arreté !')
+
+@bot.command()
+async def cal(ctx):
+    link = os.getenv('ICAL_IUT')
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as response:
+            # Vérifier que la requête est réussie
+            if response.status != 200:
+                await ctx.send('Impossible de récupérer le calendrier.')
+                return
+
+            # Analyser le contenu du lien iCal
+            content = await response.text()
+            gcal = Calendar.from_ical(content)
+
+    # Obtenez la date d'aujourd'hui
+    today = datetime.today().date()
+
+    # Parcourez les événements du calendrier pour trouver ceux qui ont lieu aujourd'hui
+    events_today = []
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            event_date = component.get('dtstart').dt.date()
+            if event_date == today:
+                events_today.append(component.get('summary'))
+
+    # Si aucun événement n'a lieu aujourd'hui, informez l'utilisateur
+    if len(events_today) == 0:
+        await ctx.send('Aucun événement aujourd\'hui.')
+    # Sinon, envoyez une liste des événements
+    else:
+        message = "Voici les événements d'aujourd'hui:\n"
+        for event in events_today:
+            message += "- " + event + "\n"
+        await ctx.send(message)
+
+@bot.command()
+async def haeyhelp(ctx):
+    # Liste des commandes disponibles
+    commands_list = [
+        ("/cal", "Affiche les événements d'aujourd'hui de l'emploi du temps de l'IUT."),
+        ("/meteo [ville]", "Affiche la météo de la ville donnée."),
+        ("/start", "Démarrer la diffusion de messages Reddit."),
+        ("/stop", "Arrêter la diffusion de messages Reddit."),
+        ("/hello", "Le bot répond 'hello' bien poliment."),
+        # Ajouter d'autres commandes ici
+    ]
+
+    # Génère une chaîne de caractères contenant les commandes disponibles
+    help_message = "Commandes disponibles:\n"
+    for command in commands_list:
+        help_message += f"```{command[0]} : {command[1]}```\n"
+
+    await ctx.send(help_message)
 
 
 bot.run(DISCROD_TOKEN)
